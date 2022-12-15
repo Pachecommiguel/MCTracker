8898caeba9ea9d331230f9c2d98e376986100ed0
Miguel Matos <miguel.matos@wit-software.com>
2022-12-15 23:26:47 +0000

    Collection#Implement ui

--- ui/src/main/java/com/example/marvelchampionstracker/ui/collection/CollectionFragment.kt
+++ ui/src/main/java/com/example/marvelchampionstracker/ui/collection/CollectionFragment.kt
@@ -1,4 +1,4 @@
-package com.example.marvelchampionstracker.ui.home
+package com.example.marvelchampionstracker.ui.collection
 
 import android.os.Bundle
 import android.util.Log
@@ -6,27 +6,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import com.example.marvelchampionstracker.app.databinding.FragmentHomeBinding
+import com.example.marvelchampionstracker.app.databinding.FragmentCollectionBinding
+import com.example.marvelchampionstracker.domain.model.CollectionModel
 import com.example.marvelchampionstracker.ui.common.AbstractBaseFragment
 import com.example.marvelchampionstracker.ui.common.BaseViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class HomeFragment : AbstractBaseFragment<HomeState>() {
+class CollectionFragment : AbstractBaseFragment<CollectionModel>() {
 
-    override val viewModel: BaseViewModel<HomeState> by viewModels<HomeViewModel>()
-    private lateinit var binding: FragmentHomeBinding
+    override val viewModel: BaseViewModel<CollectionModel> by viewModels<CollectionViewModel>()
+    private lateinit var binding: FragmentCollectionBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        binding = FragmentHomeBinding.inflate(inflater)
+        binding = FragmentCollectionBinding.inflate(inflater)
         return binding.root
     }
 
-    override fun onState(state: HomeState) {
+    override fun onState(state: CollectionModel) {
         Log.e("state ->  ", state.toString())
     }
 }
